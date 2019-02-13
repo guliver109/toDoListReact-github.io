fe7937b341d72605049bdf38dfcb1ca65ffbf7c6
Marko Gusic <markogusic@aol.com>
2019-02-13 12:56:07 -0800

    review

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import List from './List';
 
+// React Bootstrap 
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 
+const buttonStyle = {
+  margin: "10px"
+}
+const listHolder = {
+  
+  width: "100%",
+  height: "100%",
+  borderStyle: "inset"
+}
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -30,7 +41,6 @@ export default class App extends Component {
   }
 
   removeItem = (index) => {
-    // console.log(name);
     const items = this.state.items.filter((item, itemIndex) => {
       return itemIndex !== index
     })
@@ -42,16 +52,20 @@ export default class App extends Component {
       <Container>
         <Row>
           <Col>
-            <form className = "App" onSubmit = {(e) => this.addItem(e)}>
-              <input  placaholder = "Add Task" 
+            <Form className = "App" onSubmit = {(e) => this.addItem(e)}>
+              <input  placaholder = "Add Task"
                       value = {this.state.term} 
-                      onChange ={(e) => {this.updateItems(e)}}
-              />
-              <Button type = "submit">Add Task</Button>
-            </form>
+                      onChange ={(e) => {this.updateItems(e)}}></input>
+              
+              <Button style = {buttonStyle} type = "submit">Add Task</Button>
+              
+            </Form>
           </Col>
           <Col>
+            <div style = {listHolder} className = "listHolder">
+              <h5>Your List of Tasks:</h5>
             <List items = {this.state.items} removeItem = {this.removeItem} />
+            </div>
           </Col>
         </Row>
       </Container>
--- src/List.js
+++ src/List.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 // import Button from 'react-bootstrap/Button';
+const tasksList = {
 
+}
+const onClickList = {
+
+}
+const remove = {
+    
+}
 export default class List extends Component {
     render() {
         return(
-            <div>
-                <ul>
+            <div className = "tasks">
+                <ul style = {tasksList} className = "tasksList">
                     {this.props.items.map((item, index) => { 
-                        return <li onClick = {(e) => {this.props.removeItem(index)}} 
-                            key = {item} className = "remove">{item} X</li>
+                        return <li  style = {onClickList} onClick = {(e) => {this.props.removeItem(index)}} 
+                            key = {item} className = "remove" style = {remove}>{item} X</li>
                     })}
                 </ul>
             </div>
